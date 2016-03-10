0375ed14b8e0ec2afdf54e5bee68923b14bb7348
idkaaa <chris.hiles@gmail.com>
2016-03-10 14:25:23 -0500

    Init Commit

---
+++ ActorModelDemo/ActorModelDemo/Actor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ActorModelDemo.Actors
+{
+    internal abstract class Actor
+    {
+        private readonly ActionBlock<Message> p_Action;
+
+        public Actor()
+        {
+            p_Action = new ActionBlock<Message>(message =>
+            {
+                dynamic self = this;
+                dynamic mess = message;
+                self.Handle(mess);
+            });
+        }
+
+        public void p_Send(Message message)
+        {
+            p_Action.Post(message);
+        }
+    }
+}
